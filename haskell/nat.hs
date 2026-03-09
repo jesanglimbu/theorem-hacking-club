4250e8bb8dc67bd9ed5597e5986c90ab4e49b7e4
jesanglimbu <limbujesang@gmail.com>
2026-03-09 14:02:13 +0000

    Add stateful computations

--- haskell/nat.hs
+++ haskell/nat.hs
@@ -1,10 +1,30 @@
 (?) = undefined
 
+
+-- Goal:
+-- x + 10 where x is a variable
+-- defined by:
+-- Let x := 5
+-- Let x:= 5 in (x + (Let x := 4 in x))
 data Exp = Num Integer
          | Add Exp Exp
          | Mul Exp Exp
          | Ite Boolean Exp Exp
+         | Var String
+         | Let String Exp Exp -- "Let s n m" where s := n in the expression m
          deriving Show
+
+-- x + 10
+example1 :: Exp
+example1 = Add (Var "x") (Num 10)
+
+-- Let x:= 5 in x + 10
+example2 :: Exp
+example2 = Let ("x") (Num 5) (example1)
+
+-- Let x := 5 in (x + (Let x := 4 in x))
+example3 :: Exp
+example3 = Let "x" (Num 5) (Add (Var "x") (Let "x" (Num 4) (Var "x")))
 
 data Boolean = T
              | F
@@ -83,8 +103,8 @@ simplifyB (Neg a) = aux (simplifyB a)
 -- Assume we have a negation.
 -- Could we simplify (Neg a a) to Num 0?
 -- Where to go next?
-  -- 1. Lambda-calculus
-  -- 2. Stateful computation
+  -- 1. Stateful computation
+  -- 2. Lambda-calculus
 pred:: Integer -> Integer
 pred 0 = 0
 pred n = n - 1
@@ -93,12 +113,36 @@ add :: Integer -> Integer -> Integer
 add 0 m = m -- base case
 add n m = succ (add (n - 1) m ) -- recursive call
 
+type State = [(String , Integer)]
+
+initialState :: State
+initialState = []
+
+-- 'insert v n s' adds up (v,n) to the front of the list
+insert :: String -> Integer -> State -> State
+insert v n s = (v,n) : s
+
+defaultValue :: Integer
+defaultValue = 0
+
+-- for now we will simply return a defualt value
+-- if the variable is not in the state
+lup :: String -> State -> Integer
+lup v [] = defaultValue -- case where state is empty
+lup v ((w,n):s) = if v==w then n else lup v s -- case where state has one element
+
 -- interp turns an expression 'Exp' into an integer
-interp :: Exp -> Integer
-interp (Num i) = i
-interp (Add n m) = interp(n) + interp(m)
-interp (Mul n m) = interp(n) * interp(m)
-interp (Ite b n m) = if interpB(b) then interp(n) else interp(m)
+interp :: State -> Exp -> Integer
+interp state (Num i) = i
+interp state (Add n m) = interp state n + interp state m
+interp state (Mul n m) = interp state n * interp state m
+interp state (Ite b n m) = if interpB b then interp state n else interp state n
+interp state (Var x) = lup x state -- looking up the variable x in the given state
+interp state (Let x n m) =
+  let valueOfX = interp state n
+  in interp (insert x valueOfX state) m
+  
+  
 
 interpB :: Boolean -> Bool
 interpB (T) = True
@@ -109,6 +153,6 @@ interpB (Neg a) = not (interpB a)
 
 main :: IO ()
 
-main = print (interp (simplify (Ite (Neg T) (Add (Mul (Num 4) (Num 0)) (Num 10)) (Num 14))))
+main = print (interp initialState example3)
 
 
